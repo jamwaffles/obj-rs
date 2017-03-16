e637a7d7509fe5abe02cedf3d50b4658874c20a4
James Waples <jamwaffles@gmail.com>
2017-03-16 23:11:54 +0000

    Ambient lighting

--- src/main.rs
+++ src/main.rs
@@ -174,12 +174,18 @@ fn main() {
                 uniform vec3 mat_diffuse;
 
                 out vec4 f_color;
-                const vec3 LIGHT = vec3(-0.2, 0.8, 0.1);
+
+                const vec3 LIGHT = vec3(-3.0, 3.0, 4.0);
+                const vec3 LIGHT_COLOR = vec3(1.0, 1.0, 1.0);
+
                 void main() {
-                    // float lum = max(dot(normalize(v_normal), normalize(LIGHT)), 0.0);
-                    // float lum = 1.0;
-                    // vec3 color = (0.3 + 0.7 * lum) * vec3(1.0, 1.0, 1.0);
-                    f_color = vec4(mat_diffuse, 1.0);
+                    float ambientStrength = 0.1f;
+                    vec3 ambient = ambientStrength * LIGHT_COLOR;
+
+                    vec3 result = ambient * mat_ambient;
+                    f_color = vec4(result, 1.0f);
+
+                    // f_color = vec4(mat_diffuse, 1.0);
                 }
             ",
         },
